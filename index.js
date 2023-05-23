f0803fae489b5a32fe046cfca41f8e5698314425
clapitan1 <cilapitan1@gmail.com>
2023-05-22 19:03:47 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/social_network_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database', error);
+  });
+
+// User routes
+const userRoutes = require('./routes/users');
+app.use('/api/users', userRoutes);
+
+// Thought routes
+const thoughtRoutes = require('./routes/thoughts');
+app.use('/api/thoughts', thoughtRoutes);
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
